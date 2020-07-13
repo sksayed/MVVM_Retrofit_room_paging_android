8be443621da8d4141b2ab821396d05b696905869
Sheikh Sayed <sksayed@live.com>
2020-07-13 20:29:29 +0600

    Entity Note has been created

--- app/src/main/java/Model/Note.java
+++ app/src/main/java/Model/Note.java
@@ -1,4 +1,51 @@
 package Model;
 
+/*
+* to make this an entity
+* we have to declare it as an Entity
+* annotation
+* */
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
 public class Note {
+    /*
+    * it will have four fields
+    * such as id , title , text ,priority
+    * */
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title ;
+    private String text ;
+    @ColumnInfo(name = "priority_col")
+    private double priority;
+
+    public Note(String title, String text, double priority) {
+        this.title = title;
+        this.text = text;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public double getPriority() {
+        return priority;
+    }
 }
