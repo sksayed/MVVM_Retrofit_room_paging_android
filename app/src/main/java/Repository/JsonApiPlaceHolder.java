1af120038046c2351ca0498c9e47bf715e600d1b
Sheikh Sayed <sksayed@live.com>
2020-07-13 17:44:42 +0600

    Code Refactored to get allPosts

--- app/src/main/java/Repository/JsonApiPlaceHolder.java
+++ app/src/main/java/Repository/JsonApiPlaceHolder.java
@@ -2,11 +2,20 @@ package Repository;
 
 import java.util.List;
 
+import Model.Comment;
 import Model.Post;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface JsonApiPlaceHolder {
     @GET("posts")
     Call<List<Post>> getPosts ();
+    /*
+    here retro fit will map the id
+    passed in to the id of the
+    annotation
+    * */
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getCommentsFromUser (@Path("id") int userId);
 }
--- app/src/main/java/View/MainActivity.java
+++ app/src/main/java/View/MainActivity.java
@@ -32,46 +32,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit1st);
-        mTextView = (TextView) findViewById(R.id.text);
-        setUpRetrofit();
+        loadContents();
+        getPosts();
     }
 
-    private void setUpRetrofit() {
+    private void loadContents() {
+        mTextView = (TextView) findViewById(R.id.text);
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         apiPlaceHolder = retrofit.create(JsonApiPlaceHolder.class);
-
-        getPosts().enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if (!response.isSuccessful()) {
-                    mTextView.setText(response + " code is " + response.code());
-                    return;
-                }
-
-                for (Post post : response.body()) {
-                    String content = "";
-                    content += "ID: " + post.getId() + "\n";
-                    content += "User ID: " + post.getUserid() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";
-                    content += "Text: " + post.getText() + "\n\n";
-
-                    mTextView.append(content);
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-                mTextView.append(t.getMessage());
-            }
-        });
-
     }
 
-    Call<List<Post>> getPosts() {
-        return apiPlaceHolder.getPosts();
+
+    private void getPosts() {
+        apiPlaceHolder
+                .getPosts()
+                .enqueue(new Callback<List<Post>>() {
+                    @Override
+                    public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                        if (!response.isSuccessful()) {
+                            mTextView.setText(response + " code is " + response.code());
+                            return;
+                        }
+
+                        for (Post post : response.body()) {
+                            String content = "";
+                            content += "ID: " + post.getId() + "\n";
+                            content += "User ID: " + post.getUserid() + "\n";
+                            content += "Title: " + post.getTitle() + "\n";
+                            content += "Text: " + post.getText() + "\n\n";
+
+                            mTextView.append(content);
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Post>> call, Throwable t) {
+                        mTextView.append(t.getMessage());
+                    }
+                });
     }
 }
