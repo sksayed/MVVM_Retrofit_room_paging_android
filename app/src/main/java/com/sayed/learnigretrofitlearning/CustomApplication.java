1efdca4f3c2788e7c5c99953bb5af06d5673c452
Sheikh Sayed <sksayed@live.com>
2020-07-29 10:22:17 +0600

    facebook setho has been added so that database debugging is enabled

--- app/src/main/java/com/sayed/learnigretrofitlearning/CustomApplication.java
+++ app/src/main/java/com/sayed/learnigretrofitlearning/CustomApplication.java
@@ -2,11 +2,14 @@ package com.sayed.learnigretrofitlearning;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 public class CustomApplication extends Application {
     private static Application instance ;
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
         if(instance == null) {
             instance = this ;
         }
